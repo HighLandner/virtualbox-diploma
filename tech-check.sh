ceccaf15244cadde948826a23433fd3c00742e63
Yevhen Bereza <Yevhen_Bereza@epam.com>
2022-11-10 01:15:38 +0200

    Add ram, os, arch check

--- tech-check.sh
+++ tech-check.sh
@@ -13,9 +13,19 @@ check-vm() {
 	vm_params+=($vm_ram_size)
 	
 	vm_os=$(VBoxManage showvminfo $vm_id | grep "Guest OS" | awk -F ':' '{print $2}')
-	vm_params+=($vm_os)
+	vm_family=$(echo $vm_os | awk -F ' ' '{print $1}')
+	vm_params+=($vm_family)
+	vm_arch=$(echo $vm_os | awk -F ' ' '{print $2}')
+	vm_params+=($vm_arch)
 	
+}
+
+check_vm_params() {
+	if [[ "${vm_params[1]}" -eq 8192 && "${vm_params[2]}" -eq "Debian" && "${vm_params[3]}" -eq "(64-bit)"]]; then
+		echo Correct
+	fi
 }
 
 get_vm_params $vm_name
 printf '%s\n' "${vm_params[@]}"
+check_vm_params
