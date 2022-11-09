7edf91461b26d73c053ae7c83f7e32a6d0e33aea
Yevhen Bereza <Yevhen_Bereza@epam.com>
2022-11-10 00:22:26 +0200

    Add vm_params

--- tech-check.sh
+++ tech-check.sh
@@ -4,16 +4,18 @@ vm_name=$1
 
 check-vm() {
 	local vm_name=$1
+	vm_params=()
 	
 	vm_id=$(VBoxManage list runningvms | grep $vm_name | awk -F ' ' '{print $2}' | sed -r 's/[{}]//g')
-	echo $vm_id
+	vm_params+=($vm_id)
 	
 	vm_ram_size=$(VBoxManage showvminfo $vm_id | grep "Memory size" | awk -F ':' '{print $2}' | sed -r 's/[MB]//g')
-	echo $vm_ram_size
+	vm_params+=($vm_ram_size)
 	
 	vm_os=$(VBoxManage showvminfo $vm_id | grep "Guest OS" | awk -F ':' '{print $2}')
-	echo $vm_os
+	vm_params+=($vm_os)
 	
 }
 
-check-vm $vm_name
+get_vm_params $vm_name
+printf '%s\n' "${vm_params[@]}"
