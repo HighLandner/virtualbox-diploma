aa55773b4e3479d95f97673cdbed7f2b0838d554
Yevhen Bereza <Yevhen_Bereza@epam.com>
2022-11-10 00:06:13 +0200

    Update vm_os determination

--- tech-check.sh
+++ tech-check.sh
@@ -8,10 +8,10 @@ check-vm() {
 	vm_id=$(VBoxManage list runningvms | grep $vm_name | awk -F ' ' '{print $2}' | sed -r 's/[{}]//g')
 	echo $vm_id
 	
-	vm_ram_size=$(VBoxManage showvminfo $vm_id | grep "Memory size" | awk -F ':' '{print $2}' | sed -r 's/[MB]//g' )
+	vm_ram_size=$(VBoxManage showvminfo $vm_id | grep "Memory size" | awk -F ':' '{print $2}' | sed -r 's/[MB]//g')
 	echo $vm_ram_size
 	
-	vm_os=$(VBoxManage showvminfo $vm_id | grep "Guest OS" | awk -F ':' '{print $2}' | sed -r 's/[MB]//g' )
+	vm_os=$(VBoxManage showvminfo $vm_id | grep "Guest OS" | awk -F ':' '{print $2}')
 	echo $vm_os
 	
 }
